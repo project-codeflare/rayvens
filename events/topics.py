6f37d436886ab9211e9c5574d8a008f52a0b91c1
Gheorghe Teodor Bercea <gheorghe-teod.bercea@ibm.com>
2021-02-01 11:21:02 -0500

    Add counter class.

---
+++ events/topics.py
@@ -0,0 +1,12 @@
+import ray
+
+@ray.remote
+class Counter(object):
+    def __init__(self):
+        self.n = 0
+
+    def increment(self):
+        self.n += 1
+
+    def read(self):
+        return self.n
