5f1a2f3d2ce62062e77b0927f050fcc29703d364
Gheorghe Teodor Bercea <gheorghe-teod.bercea@ibm.com>
2021-02-01 10:24:19 -0500

    Initialize project.

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+with open('README.rst') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='ray-events',
+    version='0.1.0',
+    description='Ray eventing',
+    long_description=readme,
+    author='Gheorghe-Teodor Bercea',
+    author_email='gheorghe-teod.bercea@ibm.com',
+    url='https://github.ibm.com/Gheorghe-Teod-Bercea/ray-events',
+    license=license,
+    packages=find_packages(exclude=('tests', 'docs'))
+)
+
