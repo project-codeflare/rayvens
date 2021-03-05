05dc74520d8e399e0836e928d3aa92c493b9ead6
Olivier Tardieu <tardieu@users.noreply.github.com>
2021-03-05 13:59:21 -0500

    Fix regression

--- setup.py
+++ setup.py
@@ -1,17 +1,10 @@
 from setuptools import setup
 
-with open('README.md') as f:
-    readme = f.read()
-
-with open('LICENSE.txt') as f:
-    license = f.read()
-
 setup(name='rayvens',
       packages=['rayvens'],
       version='0.1',
       description='Ray eventing',
-      license=license,
-      long_description=readme,
+      license='apache-2.0',
       author='Rayvens authors',
       author_email='tardieu@us.ibm.com;gheorghe-teod.bercea@ibm.com',
       url='https://github.ibm.com/solsa/rayvens')
