6f4095317c727f1d1d4bee38815654c3e530a177
Olivier Tardieu <tardieu@users.noreply.github.com>
2021-03-12 11:43:01 -0500

    Add chdir in script

--- scripts/update-rayvens.sh
+++ scripts/update-rayvens.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+cd $(python -c 'import os,sys;print(os.path.dirname(os.path.realpath(sys.argv[1])))' "$0")
+
 docker build .. -t localhost:5000/rayvens
 docker push localhost:5000/rayvens
 ray down cluster.yaml --yes
