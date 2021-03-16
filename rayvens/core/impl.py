aa94a3710940eb9091954fae18dafff237f50905
Olivier Tardieu <tardieu@users.noreply.github.com>
2021-03-16 19:43:33 -0400

    Refactor impl.py (#41)

--- rayvens/core/impl.py
+++ rayvens/core/impl.py
@@ -8,65 +8,69 @@ from confluent_kafka import Consumer, Producer
 import threading
 
 
+# instantiate camel actor manager and setup exit hook
 def start(prefix, mode):
     camel = Camel.remote()
     atexit.register(camel.exit.remote)
     return camel
 
 
-def brokers():
-    host = os.getenv('KAFKA_SERVICE_HOST', 'localhost')
-    port = os.getenv('KAFKA_SERVICE_PORT', '31093')
-    return f'{host}:{port}'
+# construct a camel source specification from a rayvens source config
+def construct_source(config):
+    if config['kind'] is None:
+        raise TypeError('A Camel source needs a kind.')
+    if config['kind'] not in ['http-source']:
+        raise TypeError('Unsupported Camel source.')
+    url = config['url']
+    period = config.get('period', 1000)
+
+    return lambda endpoint: [{
+        'from': {
+            'uri': f'timer:tick?period={period}',
+            'steps': [{
+                'to': url
+            }, {
+                'to': endpoint
+            }]
+        }
+    }]
 
 
+# construct a camel sink specification from a rayvens source config
+def construct_sink(config):
+    if config['kind'] is None:
+        raise TypeError('A Camel sink needs a kind.')
+    if config['kind'] not in ['slack-sink']:
+        raise TypeError('Unsupported Camel sink.')
+    channel = config['channel']
+    webhookUrl = config['webhookUrl']
+
+    return lambda endpoint: [{
+        'from': {
+            'uri': endpoint,
+            'steps': [{
+                'to': f'slack:{channel}?webhookUrl={webhookUrl}',
+            }]
+        }
+    }]
+
+
+# the actor to manage camel
 @ray.remote(num_cpus=0)
 class Camel:
     def __init__(self):
         self.integrations = []
 
-    def add_source(self, name, stream, source):
-        if source['kind'] is None:
-            raise TypeError('A Camel source needs a kind.')
-        if source['kind'] not in ['http-source']:
-            raise TypeError('Unsupported Camel source.')
-        url = source['url']
-        period = source.get('period', 1000)
-
-        integration = Integration(name, [{
-            'from': {
-                'uri': f'timer:tick?period={period}',
-                'steps': [{
-                    'to': url
-                }, {
-                    'to': f'kafka:{name}?brokers=${brokers()}'
-                }]
-            }
-        }])
-
+    def add_source(self, name, stream, config):
+        spec = construct_source(config)
+        integration = Integration(name, spec)
         integration.send_to(stream)
-
         self.integrations.append(integration)
 
-    def add_sink(self, name, stream, sink):
-        if sink['kind'] is None:
-            raise TypeError('A Camel sink needs a kind.')
-        if sink['kind'] not in ['slack-sink']:
-            raise TypeError('Unsupported Camel sink.')
-        channel = sink['channel']
-        webhookUrl = sink['webhookUrl']
-
-        integration = Integration(name, [{
-            'from': {
-                'uri': f'kafka:{name}?brokers=${brokers()}',
-                'steps': [{
-                    'to': f'slack:{channel}?webhookUrl={webhookUrl}',
-                }]
-            }
-        }])
-
+    def add_sink(self, name, stream, config):
+        spec = construct_sink(config)
+        integration = Integration(name, spec)
         integration.recv_from(stream)
-
         self.integrations.append(integration)
 
     def exit(self):
@@ -74,6 +78,9 @@ class Camel:
         self.integrations = None
         for i in integrations:
             i.cancel()
+
+
+# the low-level implementation-specific stuff
 
 
 @ray.remote(num_cpus=0)
@@ -95,12 +102,18 @@ class ProducerHelper:
             self.producer.produce(self.name, data.encode('utf-8'))
 
 
+def brokers():
+    host = os.getenv('KAFKA_SERVICE_HOST', 'localhost')
+    port = os.getenv('KAFKA_SERVICE_PORT', '31093')
+    return f'{host}:{port}'
+
+
 class Integration:
-    def __init__(self, name, integration):
+    def __init__(self, name, spec):
         self.name = name
         filename = f'{name}.yaml'
         with open(filename, 'w') as f:
-            yaml.dump(integration, f)
+            yaml.dump(spec(f'kafka:{name}?brokers=${brokers()}'), f)
         command = ['kamel', 'local', 'run', filename]
         process = subprocess.Popen(command, start_new_session=True)
         self.pid = process.pid
