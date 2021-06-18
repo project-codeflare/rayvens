86c32ba0aa68aae64deac02b99fc0c225779dd7d
Olivier Tardieu <tardieu@users.noreply.github.com>
2021-06-18 14:44:55 -0400

    Implement rayvens-setup.sh --preload

--- scripts/rayvens-setup.sh
+++ scripts/rayvens-setup.sh
@@ -40,6 +40,7 @@ while [ -n "$1" ]; do
         --cpu) shift; cpu=$1;;
         --mem) shift; mem=$1;;
         --example) example="1";;
+        --preload) preload="1";;
 
         --dev)
             kind="1";
@@ -76,12 +77,62 @@ Usage: rayvens-setup.sh [options]
     --kamel-options                 options to pass to Kamel install (to configure the container registry)
     --build                         build and push custom Rayvens container image from Rayvens source code in parent folder
     --example                       generate example file "example.py" in current working directory
+    --preload                       preload main camel jars into maven repository
 
     --kind                          setup a development Kind cluster on localhost instead of deploying to current Kubernetes context
                                     (destroy existing Kind cluster if any, set Kubernetes context to Kind)
 
     --dev                           shorthand for: --registry --build --image localhost:5000/rayvens --kind --kafka --kamel
 EOF
+    exit 0
+fi
+
+if [ -n "$preload" ]; then
+    tmp=$(mktemp -d)
+    cat > "$tmp"/Preloader.java << EOF
+/*
+ * Copyright IBM Corporation 2021
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import org.apache.camel.BindToRegistry;
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+import org.apache.camel.builder.RouteBuilder;
+
+class Exit implements Processor {
+  public void process(Exchange exchange) throws Exception {
+    System.exit(0);
+  }
+}
+
+public class Preloader extends RouteBuilder {
+  @Override
+  public void configure() throws Exception {
+    from("timer:tick").to("bean:exit");
+    from("platform-http:/null").to("http:null");
+  }
+
+  @BindToRegistry
+  public Exit exit() {
+    return new Exit();
+  }
+}
+EOF
+    kamel local run "$tmp"/Preloader.java --dependency camel-k:loader-yaml
+    rm "$tmp"/Preloader.java
+    rmdir "$tmp"
     exit 0
 fi
 
