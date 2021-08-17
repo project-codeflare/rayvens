16f14f3119f35659328c67d36bb80c0c1a881916
Gheorghe Teodor Bercea <gheorghe-teod.bercea@ibm.com>
2021-08-17 12:00:03 +0300

    Remove unused field.

--- rayvens/api.py
+++ rayvens/api.py
@@ -114,7 +114,6 @@ class StreamActor:
         self._sources = {}
         self._sinks = {}
         self._latest_sent_event_timestamp = None
-        self._source_consumers = None
 
     def send_to(self, subscriber, name=None):
         if name in self._subscribers:
